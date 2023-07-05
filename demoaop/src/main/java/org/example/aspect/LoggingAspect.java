48f0035d39b37dfef962ef319dc06ff98f2310b3
ihab abadi <ihab@utopios.net>
2023-07-05 16:03:51 +0200

    debut aop

--- demoaop/src/main/java/org/example/aspect/LoggingAspect.java
+++ demoaop/src/main/java/org/example/aspect/LoggingAspect.java
@@ -1,16 +1,30 @@
 package org.example.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class LoggingAspect {
 
-    //@Before("execution(* *.(..))")
-    @After("@annotation(org.example.aspect.Loggable)")
+    @Before("execution(* *.(..))")
     public void loggingBefore(JoinPoint joinPoint) {
         System.out.println("Method Invoked "+ joinPoint.getSignature().getName());
+    }
+
+    @After("execution(* *.(..))")
+    //@Around("@annotation(org.example.aspect.Loggable)")
+    public void loggingAfter(JoinPoint joinPoint) {
+        System.out.println("Method Invoked "+ joinPoint.getSignature().getName());
+    }
+
+    @Around("@annotation(org.example.aspect.Loggable)")
+    public void loggingAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("Before ");
+        joinPoint.proceed();
+        System.out.println("After");
     }
 }
